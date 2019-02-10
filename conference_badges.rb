5b41824aac85b5a9c47b4196442863aafdfe8c3b
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 15:42:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,8 @@
 # Write your code here.
 def badge_maker
-  
-end
-
-def batch_badge_creator(array)
   message_array = []
   array.each do |x|
     message_array.push("Hello, my name is #{x}.")
   end
 end
+
