2b7368063479ad887639414002cb868c833ab053
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 15:53:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,10 +12,8 @@ def batch_badge_creator(people)
 end
 
 def assign_rooms(people)
-  n=1
-  people.each do |person|
-    "Hello, #{person}! You'll be assigned to room #{n}"
-    n+=1
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
